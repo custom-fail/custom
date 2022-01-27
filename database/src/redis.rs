b076f58f7ad4ab5fcd021e6bb1e11082ca949508
oceaann <47253870+oceaann@users.noreply.github.com>
2022-01-27 21:56:39 +0100

    redis fetching user score and position

--- database/src/redis.rs
+++ database/src/redis.rs
@@ -1,4 +1,6 @@
-use redis::{Client, RedisError};
+use redis::{Client, Commands, RedisError};
+use twilight_model::id::Id;
+use twilight_model::user::User;
 
 pub struct RedisConnection {
     client: Client
@@ -11,6 +13,14 @@ impl RedisConnection {
         Ok(Self {
             client
         })
+    }
+
+    pub fn get_by_user(&self, path: String, user_id: Id<User>) -> Result<(u32, u32), RedisError> {
+        let mut connection = self.client.get_connection()?;
+        let user_id = user_id.to_string();
+        let score = connection.zscore(path.clone(), user_id.clone())?;
+        let position = connection.zrevrank(path, user_id)?;
+        Ok((score, position))
     }
 
 }
