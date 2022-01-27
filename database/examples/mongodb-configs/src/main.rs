fb5f3db9f517c670d832997c0cbbb2b9a43928cc
oceaann <47253870+oceaann@users.noreply.github.com>
2022-01-27 09:43:38 +0100

    Add fetching case to examples

--- database/examples/mongodb-configs/src/main.rs
+++ database/examples/mongodb-configs/src/main.rs
@@ -2,7 +2,9 @@ use std::str::FromStr;
 use dotenv::dotenv;
 use twilight_model::guild::Guild;
 use twilight_model::id::Id;
+use twilight_model::user::User;
 use database::mongodb::MongoDBConnection;
+use mongodb::bson::doc;
 
 #[tokio::main]
 async fn main() {
@@ -15,9 +17,18 @@ async fn main() {
     println!("Connected to mongodb-configs database");
 
     let guild_id = Id::<Guild>::from_str("898986393177567242").unwrap();
+    let user_id = Id::<User>::from_str("494386855974928386").unwrap();
 
     for _ in 0..10 {
         println!("{:?}", connection.get_config(guild_id.clone()).await.unwrap());
     }
+
+    let guild_id = guild_id.to_string();
+    let member_id = user_id.to_string();
+
+    let response = connection.cases.find_one(
+        doc! { "member_id": member_id, "guild_id": guild_id },
+        None).await.unwrap();
+    println!("{:?}", response);
 
 }
