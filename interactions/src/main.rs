6a62656086b4a63daaeaa0b1da6472ee454f95ec
oceaann <47253870+oceaann@users.noreply.github.com>
2022-01-27 22:30:23 +0100

    connect to mongodb and redis

--- interactions/src/main.rs
+++ interactions/src/main.rs
@@ -1,4 +1,16 @@
+use database::mongodb::MongoDBConnection;
+use database::redis::RedisConnection;
+use dotenv::dotenv;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+
+    dotenv();
+
+    let mongodb_url = std::env::var("MONGODB_URL").expect("Cannot load MONGODB_URL from .env");
+    let redis_url = std::env::var("REDIS_URL").expect("Cannot load REDIS_URL from .env");
+
+    let mongodb = MongoDBConnection::connect(mongodb_url).await.unwrap();
+    let redis = RedisConnection::connect(redis_url).unwrap();
+
 }
