96d88064434e3d8e1a8404c1a0b983b277323ee0
oceaann <47253870+oceaann@users.noreply.github.com>
2022-01-28 10:12:41 +0100

    make dotenv not emitting warn

--- interactions/src/main.rs
+++ interactions/src/main.rs
@@ -7,7 +7,7 @@ use dotenv::dotenv;
 #[tokio::main]
 async fn main() {
 
-    dotenv();
+    dotenv().ok();
 
     let mongodb_url = std::env::var("MONGODB_URL").expect("Cannot load MONGODB_URL from .env");
     let redis_url = std::env::var("REDIS_URL").expect("Cannot load REDIS_URL from .env");
