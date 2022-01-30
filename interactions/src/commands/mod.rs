edc6f1b7bd6c4578ea3911754085af748d0d27b0
oceaann <47253870+oceaann@users.noreply.github.com>
2022-01-30 14:14:13 +0100

    Slash command to text parser

--- interactions/src/commands/mod.rs
+++ interactions/src/commands/mod.rs
@@ -34,3 +34,27 @@ impl Clone for Command {
         }
     }
 }
+
+pub fn parse_slash_command_to_text(slash_command_data: CommandData) -> String {
+    if slash_command_data.options.len() != 0 {
+        handle_value(
+            slash_command_data.name,
+            slash_command_data.options[0].name.to_owned(),
+            slash_command_data.options[0].value.to_owned(),
+        )
+    } else {
+        slash_command_data.name
+    }
+}
+
+fn handle_value(before: String, name: String, slash_command_value: CommandOptionValue) -> String {
+    match slash_command_value {
+        SubCommandGroup(value) => handle_value(
+            format!("{} {}", before, name),
+            value[0].name.to_owned(),
+            value[0].value.to_owned(),
+        ),
+        SubCommand(_) => format!("{} {}", before, name),
+        _ => before,
+    }
+}
