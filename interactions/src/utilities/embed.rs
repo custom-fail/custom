a969937539b66dbc64ec25fc1c56a856beae1600
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-01 17:49:41 +0100

    response_from_embed_fields method

--- interactions/src/utilities/embed.rs
+++ interactions/src/utilities/embed.rs
@@ -1,5 +1,5 @@
 use twilight_model::application::callback::CallbackData;
-use twilight_model::channel::embed::Embed;
+use twilight_model::channel::embed::{Embed, EmbedField};
 
 fn text_to_embed(title: String, description: String) -> Embed {
     Embed {
@@ -39,4 +39,22 @@ pub fn embed_to_response(embed: Embed) -> CallbackData {
         flags: None,
         tts: None
     }
+}
+
+pub fn response_from_embed_fields(fields: Vec<EmbedField>) -> CallbackData {
+    embed_to_response(Embed {
+        author: None,
+        color: None,
+        description: None,
+        fields,
+        footer: None,
+        image: None,
+        kind: "".to_string(),
+        provider: None,
+        thumbnail: None,
+        timestamp: None,
+        title: None,
+        url: None,
+        video: None
+    })
 }
