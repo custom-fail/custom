1766b524cfe1752aa355eef9e2d825217efe53f3
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-01 10:14:10 +0100

    get_avatar_from_member method

--- database/src/models/case.rs
+++ database/src/models/case.rs
@@ -1,7 +1,11 @@
 use twilight_model::datetime::Timestamp;
 use serde::{Serialize, Deserialize};
+use twilight_http::response::marker::MemberBody;
+use twilight_model::channel::embed::{Embed, EmbedAuthor};
+use twilight_model::guild::Member;
 use twilight_model::id::Id;
 use twilight_model::id::marker::{GuildMarker, UserMarker};
+use twilight_model::util::ImageHash;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Case {
@@ -14,4 +18,14 @@ pub struct Case {
     pub removed: bool,
     pub duration: Option<String>,
     pub index: u16
+}
+
+fn get_avatar_from_member(member: Member) -> String {
+    match member.avatar {
+        Some(avatar) => {
+            let file_format = if avatar.is_animated() { "gif" } else { "png" };
+            format!("https://cdn.discordapp.com/avatars/{}/{}.{}", member.user.id, avatar, file_format)
+        }
+        None =>  "https://cdn.discordapp.com/embed/avatars/0.png".to_string()
+    }
 }
