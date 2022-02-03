df12753dfe97e6d5568e01a86d3d7712e70b784d
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-03 15:12:01 +0100

    RedisConnection::increase method

--- database/src/redis.rs
+++ database/src/redis.rs
@@ -1,27 +1,36 @@
 use redis::{Client, Commands, RedisError};
-use twilight_model::id::Id;
 use twilight_model::id::marker::UserMarker;
+use twilight_model::id::Id;
 
 pub struct RedisConnection {
-    client: Client
+    client: Client,
 }
 
 impl RedisConnection {
-
     pub fn connect(url: String) -> Result<Self, RedisError> {
         let client = Client::open(url)?;
-        Ok(Self {
-            client
-        })
+        Ok(Self { client })
     }
 
-    pub fn get_by_position(&self, path: String, position: usize) -> Result<Option<u32>, RedisError> {
+    pub fn get_by_position(
+        &self,
+        path: String,
+        position: usize,
+    ) -> Result<Option<u32>, RedisError> {
         let mut connection = self.client.get_connection()?;
-        let result: Vec<u32> = connection.zrevrange_withscores(path, (position - 1) as isize, (position - 1) as isize)?;
+        let result: Vec<u32> = connection.zrevrange_withscores(
+            path,
+            (position - 1) as isize,
+            (position - 1) as isize,
+        )?;
         Ok(result.first().cloned())
     }
 
-    pub fn get_by_user(&self, path: String, user_id: Id<UserMarker>) -> Result<(u32, u32), RedisError> {
+    pub fn get_by_user(
+        &self,
+        path: String,
+        user_id: Id<UserMarker>,
+    ) -> Result<(u32, u32), RedisError> {
         let mut connection = self.client.get_connection()?;
         let user_id = user_id.to_string();
         let score = connection.zscore(path.clone(), user_id.clone())?;
@@ -34,12 +43,21 @@ impl RedisConnection {
         Ok(connection.zrevrange_withscores(path, 0, limit - 1)?)
     }
 
+    pub fn increase(
+        &self,
+        path: String,
+        user_id: Id<UserMarker>,
+        count: u8,
+    ) -> Result<(), RedisError> {
+        let mut connection = self.client.get_connection()?;
+        Ok(connection.zincr(path, user_id.to_string(), count)?)
+    }
 }
 
 impl Clone for RedisConnection {
     fn clone(&self) -> Self {
         Self {
-            client: self.client.clone()
+            client: self.client.clone(),
         }
     }
 }
