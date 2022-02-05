2e952f28d75f0738c533c57bef56034bcf60c0e9
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-05 23:51:50 +0100

    (fix): add intents

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,6 +1,7 @@
 mod events;
 mod modules;
 
+use std::path::Iter;
 use crate::events::on_event;
 use database::mongodb::MongoDBConnection;
 use database::redis::RedisConnection;
@@ -24,7 +25,7 @@ async fn main() {
         std::env::var("DISCORD_TOKEN").expect("Cannot load DISCORD_TOKEN from .env");
     let discord_http = Arc::new(twilight_http::Client::new(discord_token.clone()));
 
-    let intents = Intents::GUILD_MESSAGES | Intents::GUILDS;
+    let intents = Intents::GUILD_MESSAGES | Intents::GUILDS | Intents::GUILD_BANS | Intents::GUILD_MEMBERS;
     let (shard, mut events) = Shard::new(discord_token, intents);
 
     shard.start().await.expect("Failed to start shard");
