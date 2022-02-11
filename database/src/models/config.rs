7c7febc1011002db6dc157b7f858affb1901a751
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-11 16:44:02 +0100

    automod part in GuildConfig

--- database/src/models/config.rs
+++ database/src/models/config.rs
@@ -3,60 +3,68 @@ use serde::{Serialize, Deserialize};
 use twilight_model::id::Id;
 use twilight_model::id::marker::{ChannelMarker, GuildMarker};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Action {
+    pub delete_message: bool,
+    pub duration: Option<u64>,
+    pub r#type: Option<u8>
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum AutoModeratorType {
+    MessageLength = 1,
+    AntiCapsLock = 2
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct MessageLength {
+    pub max_lines: u16,
+    pub line_len: u8,
+    pub first_action: String,
+    pub repeat_action: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AntiCapsLock {
+    pub max_uppercase: u8,
+    pub min_msg_len: u16,
+    pub max_msg_len: u16,
+    pub first_action: String,
+    pub repeat_action: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "type")]
+pub enum AutoModerator {
+    MessageLength(MessageLength),
+    AntiCapsLock(AntiCapsLock)
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Moderation {
     pub mute_type: u8,
     pub native_support: bool,
     pub logs_channel: Option<Id<ChannelMarker>>,
-    pub dm_case: bool
+    pub dm_case: bool,
+    pub automod_actions: HashMap<String, Action>,
+    pub automod: Vec<AutoModerator>
 }
 
-impl Moderation {
-    fn clone(&self) -> Self {
-        Self {
-            mute_type: self.mute_type.clone(),
-            native_support: self.native_support.clone(),
-            logs_channel: self.logs_channel.clone(),
-            dm_case: self.dm_case.clone()
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Levels {
     pub xp_timeout: u16,
     pub xp_min: u8,
     pub xp_max: u8
 }
 
-impl Clone for Levels {
-    fn clone(&self) -> Self {
-        Self {
-            xp_timeout: self.xp_timeout.clone(),
-            xp_min: self.xp_min.clone(),
-            xp_max: self.xp_max.clone()
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Top {
     pub week: bool,
     pub day: bool,
     pub webhook_url: String
 }
 
-impl Clone for Top {
-    fn clone(&self) -> Self {
-        Self {
-            week: self.week.clone(),
-            day: self.day.clone(),
-            webhook_url: self.webhook_url.clone()
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct GuildConfig {
     pub guild_id: Id<GuildMarker>,
     pub enabled: HashMap<String, bool>,
@@ -64,17 +72,4 @@ pub struct GuildConfig {
     pub premium: bool,
     pub levels: Levels,
     pub top: Top
-}
-
-impl Clone for GuildConfig {
-    fn clone(&self) -> Self {
-        Self {
-            guild_id: self.guild_id.clone(),
-            enabled: self.enabled.clone(),
-            moderation: self.moderation.clone(),
-            premium: self.premium.clone(),
-            levels: self.levels.clone(),
-            top: self.top.clone()
-        }
-    }
 }
