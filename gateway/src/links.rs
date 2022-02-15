1e06f8d34a4f1b73fc885b351443558695f5f1d4
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-15 14:22:33 +0100

    ScamLinks manager init

---
+++ gateway/src/links.rs
@@ -0,0 +1,21 @@
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+const ALL_DOMAINS_ENDPOINT: &str = "https://phish.sinking.yachts/v2/all";
+
+pub struct ScamLinks {
+    discord_scam_domains: Arc<Mutex<Vec<String>>>
+}
+
+impl ScamLinks {
+    pub async fn new() -> Result<Self, String> {
+
+        let links = reqwest::get(ALL_DOMAINS_ENDPOINT).await.map_err(|err| format!("{err}"))?;
+        let links: Vec<String> = serde_json::from_str(links.text().await.map_err(|err| format!("{err}"))?.as_str()).map_err(|err| format!("{err}"))?;
+
+        Ok(Self {
+            discord_scam_domains: Arc::new(Mutex::new(links))
+        })
+
+    }
+}
