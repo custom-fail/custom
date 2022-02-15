d9874232eb17f52174f606da243c7c8ff028b037
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-15 15:51:29 +0100

    (fix): links => domains

--- gateway/src/links.rs
+++ gateway/src/links.rs
@@ -10,11 +10,11 @@ pub struct ScamLinks {
 impl ScamLinks {
     pub async fn new() -> Result<Self, String> {
 
-        let links = reqwest::get(ALL_DOMAINS_ENDPOINT).await.map_err(|err| format!("{err}"))?;
-        let links: Vec<String> = serde_json::from_str(links.text().await.map_err(|err| format!("{err}"))?.as_str()).map_err(|err| format!("{err}"))?;
+        let domains = reqwest::get(ALL_DOMAINS_ENDPOINT).await.map_err(|err| format!("{err}"))?;
+        let domains: Vec<String> = serde_json::from_str(domains.text().await.map_err(|err| format!("{err}"))?.as_str()).map_err(|err| format!("{err}"))?;
 
         Ok(Self {
-            discord_scam_domains: Arc::new(Mutex::new(links))
+            discord_scam_domains: Arc::new(Mutex::new(domains))
         })
 
     }
