ef46135b6223f3c7be4cab5230ac85db0858e64a
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-17 19:12:28 +0100

    (fix): remove unused functions

--- interactions/src/commands/case/mod.rs
+++ interactions/src/commands/case/mod.rs
@@ -1,15 +1,5 @@
-use twilight_model::application::interaction::application_command::InteractionMember;
-use twilight_model::application::interaction::ApplicationCommand;
-use twilight_model::id::Id;
-use twilight_model::id::marker::UserMarker;
-
 pub mod last;
 pub mod details;
 pub mod list;
 pub mod remove;
 pub mod edit;
-
-pub fn get_member_from_command_data(interaction: Box<ApplicationCommand>) -> Result<(Id<UserMarker>, InteractionMember), String> {
-    Ok(Vec::from_iter(interaction.data.resolved.ok_or("No member specified".to_string())?
-        .members.into_iter()).first().cloned().ok_or("Cannot find member information")?)
-}
