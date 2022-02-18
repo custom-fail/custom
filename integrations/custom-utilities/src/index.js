5e5b7501506f91c7812cdefbb942103dfc41933d
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-18 23:44:44 +0100

    custom-utilities init

---
+++ integrations/custom-utilities/src/index.js
@@ -0,0 +1,14 @@
+import { Client } from "eris"
+import "dotenv/config"
+
+import "./handlers/modules.js"
+
+export const client = new Client(process.env.DISCORD_TOKEN)
+
+export const configs = new Map()
+configs.set("898986393177567242", {
+
+})
+
+await client.connect()
+
