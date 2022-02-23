05f94c04dc7cf89f925675371d1e27d79387d311
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-23 23:45:09 +0100

    module for auto message deleting

--- integrations/custom-utilities/src/index.js
+++ integrations/custom-utilities/src/index.js
@@ -7,7 +7,11 @@ export const client = new Client(process.env.DISCORD_TOKEN)
 
 export const configs = new Map()
 configs.set("898986393177567242", {
-    autoPublisherChannels: ["946159295270096997"]
+    autoPublisherChannels: ["946159295270096997"],
+    autoDeleteChannels: {
+        "904330935871356949": 10000
+    },
+    voiceRole: "946173995768774687"
 })
 
 await client.connect()
