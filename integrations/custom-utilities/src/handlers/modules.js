40024d4b2f28abb5dc13561a90e8f65b020fe23a
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-23 22:53:42 +0100

    (fix): modules import path

--- integrations/custom-utilities/src/handlers/modules.js
+++ integrations/custom-utilities/src/handlers/modules.js
@@ -1,6 +1,6 @@
 import { readdirSync } from "fs"
 
-const folders = readdirSync("src/modules/");
+const folders = readdirSync("src/modules/")
 folders.forEach(async (file) => {
-    await import(`../${file}`)
+    await import(`../modules/${file}`)
 })
