3ed8796985dfc4610c9173747ce6184696abb579
oceaann <47253870+oceaann@users.noreply.github.com>
2022-02-23 23:45:50 +0100

    on voice channel role module

---
+++ integrations/custom-utilities/src/modules/voiceRole.js
@@ -0,0 +1,13 @@
+import { client, configs } from "../index.js";
+
+client.on("voiceChannelJoin", async (member) => {
+    const config = configs.get(member.guild?.id)
+    if(!config || !config.voiceRole) return
+    console.log(await member.addRole(config.voiceRole))
+})
+
+client.on("voiceChannelLeave", async (member) => {
+    const config = configs.get(member.guild?.id)
+    if(!config || !config.voiceRole) return
+    await member.removeRole(config.voiceRole)
+})
