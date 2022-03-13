033772357cbbd5d30c56394071aae86f9b060402
oceaann <47253870+oceaann@users.noreply.github.com>
2022-03-13 18:40:44 +0100

    (fix): remove ignored results warnings

--- gateway/src/events.rs
+++ gateway/src/events.rs
@@ -3,22 +3,24 @@ use database::redis::RedisConnection;
 use std::sync::Arc;
 use twilight_http::Client;
 use twilight_model::gateway::event::Event;
+use crate::ScamLinks;
 
 pub async fn on_event(
     event: Event,
     mongodb: MongoDBConnection,
     redis: RedisConnection,
     discord_http: Arc<Client>,
+    scam_domains: ScamLinks
 ) -> Result<(), ()> {
     match event {
         Event::MessageCreate(event) => {
-            crate::modules::auto::run(event.clone(), mongodb.clone(), discord_http).await;
-            crate::modules::top::run(event, mongodb, redis).await;
+            crate::modules::auto::run(event.clone(), mongodb.clone(), discord_http, scam_domains).await.ok();
+            crate::modules::top::run(event, mongodb, redis).await.ok();
         }
-        Event::BanAdd(event) => { crate::modules::case::on_ban::run(event, mongodb, discord_http).await; },
-        Event::MemberRemove(event) => { crate::modules::case::on_kick::run(event, mongodb, discord_http).await; },
+        Event::BanAdd(event) => { crate::modules::case::on_ban::run(event, mongodb, discord_http).await.ok(); },
+        Event::MemberRemove(event) => { crate::modules::case::on_kick::run(event, mongodb, discord_http).await.ok(); },
         Event::MemberUpdate(event) => {
-            crate::modules::case::on_timeout::run(event, mongodb, discord_http).await;
+            crate::modules::case::on_timeout::run(event, mongodb, discord_http).await.ok();
         }
         _ => return Err(()),
     };
