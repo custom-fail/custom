42bf35af740c0b6830e2b814292ebe11a0bfb1d0
ocean <kontakt.ocaen@gmail.com>
2022-05-15 13:51:22 +0200

    check position of moderator and target roles in moderation commands

--- interactions/src/commands/moderation/execute.rs
+++ interactions/src/commands/moderation/execute.rs
@@ -4,10 +4,14 @@ use chrono::Utc;
 use humantime::Duration;
 use mongodb::bson::DateTime;
 use twilight_http::Client;
+use twilight_http::error::ErrorType;
 use twilight_model::application::interaction::application_command::CommandOptionValue;
 use twilight_model::channel::message::MessageFlags;
 use twilight_model::datetime::Timestamp;
+use twilight_model::guild::{Member, PartialMember};
 use twilight_model::http::interaction::{InteractionResponseData, InteractionResponseType};
+use twilight_model::id::Id;
+use twilight_model::id::marker::{GuildMarker, UserMarker};
 use database::models::case::Case;
 use database::models::config::GuildConfig;
 use database::mongodb::MongoDBConnection;
@@ -18,6 +22,56 @@ use utils::modals::{ModalBuilder, RepetitiveTextInput};
 use utils::uppercase::FirstLetterToUpperCase;
 use crate::commands::ResponseData;
 use crate::InteractionContext;
+
+async fn get_target_member(
+    discord_http: &Arc<Client>,
+    guild_id: Id<GuildMarker>,
+    member_id: Id<UserMarker>
+) -> Result<Option<Member>, Error> {
+    match discord_http.guild_member(guild_id, member_id).exec().await {
+        Ok(value) => match value.model().await {
+            Ok(member) => Ok(Some(member)),
+            Err(err) => return Err(Error::from(err))
+        }
+        Err(err) => {
+            return match err.kind() {
+                ErrorType::Response { status, .. } => {
+                    if status == &404 { Ok(None) } else { Err(Error::from(err)) }
+                },
+                _ => Err(Error::from(err))
+            }
+        }
+    }
+}
+
+pub fn check_position(
+    redis: &RedisConnection,
+    guild_id: Id<GuildMarker>,
+    target_member: Member,
+    member: PartialMember
+) -> Result<bool, Error> {
+
+    let target_role = match target_member.roles.first() {
+        Some(role) => role,
+        None => return Ok(true)
+    };
+
+    let moderator_role = match member.roles.first() {
+        Some(role) => role,
+        None => return Ok(false)
+    };
+
+    let guild = redis.get_guild(guild_id).map_err(Error::from)?;
+    let everyone_position = guild.roles.len();
+
+    let target_role_index = guild.roles.iter()
+        .position(|role| role == target_role).unwrap_or(everyone_position);
+    let moderator_role_index = guild.roles.iter()
+        .position(|role| role == moderator_role).unwrap_or(everyone_position);
+
+    Ok(target_role_index > moderator_role_index)
+
+}
 
 pub async fn run(
     interaction: InteractionContext,
@@ -51,8 +105,9 @@ pub async fn run(
 
     let user_id = interaction.user.ok_or("Cannot find executor")?.id;
     let guild_id = interaction.guild_id.ok_or("This command is guild only")?;
+    let member = interaction.member.ok_or("This command is guild only")?;
 
-    let member_id = *check_type!(
+    let target_id = *check_type!(
         interaction.options.get("member").ok_or("There is no member id")?,
         CommandOptionValue::User
     ).ok_or("Member id type not match")?;
@@ -74,12 +129,26 @@ pub async fn run(
         _ => return Err(Error::from("Invalid action"))
     };
 
+    if target_id != user_id {
+        let target_member = get_target_member(
+            &discord_http, guild_id, target_id
+        ).await.map_err(Error::from)?;
+
+        if let Some(target_member) = target_member {
+            if !check_position(&redis, guild_id, target_member, member)? {
+                return Err(
+                    Error::from("Missing Permissions: Cannot execute moderation action on user with higher role")
+                )
+            }
+        }
+    }
+
     let mut case_duration = None;
     if *&interaction.command_text == "mute" {
         let duration = check_type!(
-                interaction.options.get("duration").ok_or("There is no duration")?,
-               CommandOptionValue::String
-            ).ok_or("Duration type not match")?.to_owned();
+            interaction.options.get("duration").ok_or("There is no duration")?,
+            CommandOptionValue::String
+        ).ok_or("Duration type not match")?.to_owned();
 
         let duration = Duration::from_str(duration.as_str())
             .map_err(|_| "Invalid duration string (try 3m, 10s, 2d)")?;
@@ -89,7 +158,7 @@ pub async fn run(
         case_duration = Some(duration.as_secs() as i64);
 
         discord_http
-            .update_guild_member(guild_id, member_id)
+            .update_guild_member(guild_id, target_id)
             .communication_disabled_until(timestamp)
             .map_err(Error::from)?
             .exec().await.map_err(Error::from)?
@@ -102,7 +171,7 @@ pub async fn run(
         moderator_id: user_id,
         created_at: DateTime::now(),
         guild_id,
-        member_id,
+        member_id: target_id,
         action: case_type,
         reason,
         removed: false,
@@ -112,10 +181,10 @@ pub async fn run(
 
     let result_action = match interaction.command_text.as_str() {
         "kick" => {
-            discord_http.remove_guild_member(guild_id, member_id).exec().await.err()
+            discord_http.remove_guild_member(guild_id, target_id).exec().await.err()
         },
         "ban" => {
-            discord_http.create_ban(guild_id, member_id).exec().await.err()
+            discord_http.create_ban(guild_id, target_id).exec().await.err()
         },
         _ => None
     };
@@ -123,9 +192,9 @@ pub async fn run(
     let case_embed = case.to_embed(discord_http.to_owned()).await?;
 
     let result_case = mongodb.create_case(
-        discord_http, redis, case,
+        discord_http.to_owned(), redis, case,
         case_embed.to_owned(),
-        if config.moderation.dm_case { Some(member_id) } else { None },
+        if config.moderation.dm_case { Some(target_id) } else { None },
         config.moderation.logs_channel
     ).await.err();
 
