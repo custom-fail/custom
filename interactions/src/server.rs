bcec52caee165475af19b5c2b0c184efad05e418
ocean <kontakt.ocaen@gmail.com>
2022-06-08 17:30:18 +0200

    implement for interactions module

--- interactions/src/server.rs
+++ interactions/src/server.rs
@@ -1,11 +1,10 @@
 use std::convert::Infallible;
-use std::sync::Arc;
+use database::clients::DiscordClients;
 use ed25519_dalek::PublicKey;
 use hyper::{Body, Method, Request, Response, StatusCode};
 use hyper::http::HeaderValue;
 use hyper::service::{make_service_fn, service_fn};
 use serde_json::json;
-use twilight_http::Client;
 use twilight_model::application::interaction::Interaction;
 use database::mongodb::MongoDBConnection;
 use database::redis::RedisConnection;
@@ -29,11 +28,11 @@ struct HttpResponse {
 }
 
 impl HttpResponse {
-    pub fn to_response(&self) -> Result<Response<Body>, Infallible> {
+    pub fn to_response(&self) -> Response<Body> {
         let mut response = Response::default();
         *response.status_mut() = self.status;
         *response.body_mut() = Body::from(self.body);
-        Ok(response)
+        response
     }
 }
 
@@ -43,62 +42,86 @@ const MISSING_HEADERS: HttpResponse = HttpResponse { body:"Missing headers", sta
 const UNAUTHORIZED: HttpResponse = HttpResponse { body: "Unauthorized", status: StatusCode::UNAUTHORIZED };
 const INVALID_BODY: HttpResponse = HttpResponse { body: "Invalid/Missing body", status: StatusCode::BAD_REQUEST };
 
-async fn route(request: Request<Body>, public_key: PublicKey, application: Application, mongodb: MongoDBConnection, redis: RedisConnection, discord_http: Arc<Client>) -> Result<Response<Body>, Infallible> {
+async fn route(
+    request: Request<Body>,
+    application: Application,
+    mongodb: MongoDBConnection,
+    redis: RedisConnection,
+    discord_clients: DiscordClients
+) -> Result<Response<Body>, Response<Body>> {
 
     if request.method() != Method::POST {
-        return METHOD_NOT_ALLOWED.to_response();
+        return Ok(METHOD_NOT_ALLOWED.to_response());
     };
 
     let timestamp = request.headers().get("X-Signature-Timestamp");
     let signature = request.headers().get("X-Signature-Ed25519");
 
-    let timestamp = match string_from_headers_option(timestamp) {
-        Some(timestamp) => timestamp,
-        None => return MISSING_HEADERS.to_response()
-    };
+    let timestamp = string_from_headers_option(timestamp)
+        .ok_or(MISSING_HEADERS.to_response())?;
 
-    let signature = match string_from_headers_option(signature) {
-        Some(signature) => signature,
-        None => return MISSING_HEADERS.to_response()
-    };
+    let signature = string_from_headers_option(signature)
+        .ok_or(MISSING_HEADERS.to_response())?;
 
     let whole_body = hyper::body::to_bytes(request.into_body()).await;
-    let whole_body = match whole_body {
-        Ok(whole_body) => whole_body,
-        Err(_) => return INVALID_BODY.to_response()
-    };
+    let whole_body = whole_body.map_err(|_| INVALID_BODY.to_response())?;
 
     let reversed_body = whole_body.iter().cloned().collect::<Vec<u8>>();
-    let body = String::from_utf8(reversed_body);
-    let body = match body {
-        Ok(body) => body,
-        Err(_) => return INVALID_BODY.to_response()
-    };
+    let body = String::from_utf8(reversed_body).map_err(|_| INVALID_BODY.to_response())?;
+
+    let interaction = serde_json::from_str::<Interaction>(body.as_str())
+        .map_err(|_| INVALID_BODY.to_response())?;
+
+    let application_id = interaction.application_id();
+    let client = discord_clients.get(&application_id)
+        .ok_or(UNAUTHORIZED.to_response())?;
+
+    let pbk_bytes = hex::decode(client.public_key.as_str())
+        .map_err(|_| INTERNAL_SERVER_ERROR.to_response())?;
+    let public_key = PublicKey::from_bytes(&pbk_bytes)
+        .map_err(|_| INTERNAL_SERVER_ERROR.to_response())?;
 
     if !verify_signature(public_key, signature, timestamp, body.clone()) {
-        return UNAUTHORIZED.to_response();
+        return Ok(UNAUTHORIZED.to_response());
     };
 
-    let interaction = match serde_json::from_str::<Interaction>(body.as_str()) {
-        Ok(value) => value,
-        Err(_) => return INVALID_BODY.to_response()
-    };
-
-    let content = handle_interaction(interaction, application, mongodb, redis, discord_http).await;
+    let content = handle_interaction(
+        interaction, application, mongodb, redis, client.http.to_owned()
+    ).await;
     let content = json!(content).to_string();
 
     let response = Response::builder()
         .header("Content-Type", "application/json")
         .body(Body::from(content));
 
-    match response {
-        Ok(response) => Ok(response),
-        Err(_) => INTERNAL_SERVER_ERROR.to_response()
-    }
+    response.map_err(|_| INTERNAL_SERVER_ERROR.to_response())
 
 }
 
-pub async fn listen(port: u8, public_key: PublicKey, application: Application, mongodb: MongoDBConnection, redis: RedisConnection, discord_http: Arc<Client>) {
+pub async fn run_route(
+    request: Request<Body>,
+    application: Application,
+    mongodb: MongoDBConnection,
+    redis: RedisConnection,
+    discord_clients: DiscordClients
+) -> Result<Response<Body>, Infallible> {
+    let response = route(
+        request, application, mongodb, redis, discord_clients
+    ).await;
+
+    Ok(match response {
+        Ok(response) => response,
+        Err(response) => response
+    })
+}
+
+pub async fn listen(
+    port: u8,
+    application: Application,
+    mongodb: MongoDBConnection,
+    redis: RedisConnection,
+    discord_http: DiscordClients
+) {
 
     let service = make_service_fn(move |_| {
         let application = application.clone();
@@ -107,7 +130,13 @@ pub async fn listen(port: u8, public_key: PublicKey, application: Application, m
         let discord_http = discord_http.clone();
         async move {
             Ok::<_, hyper::Error>(service_fn(move |req: Request<Body>| {
-                route(req, public_key.to_owned(), application.to_owned(), mongodb.to_owned(), redis.to_owned(), discord_http.to_owned())
+                run_route(
+                    req,
+                    application.to_owned(),
+                    mongodb.to_owned(),
+                    redis.to_owned(),
+                    discord_http.to_owned()
+                )
             }))
         }
     });
