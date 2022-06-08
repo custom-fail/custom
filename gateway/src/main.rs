0aff1585a2e87d9b6d81279019e413caf9908b60
ocean <kontakt.ocaen@gmail.com>
2022-06-08 21:23:36 +0200

    implement for gateway module

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -3,16 +3,44 @@ mod modules;
 mod links;
 mod bucket;
 
+use std::sync::Arc;
 use crate::events::on_event;
 use database::mongodb::MongoDBConnection;
 use database::redis::RedisConnection;
 use dotenv::dotenv;
 use futures_util::StreamExt;
-use std::sync::Arc;
+use database::clients::{Client, DiscordClients, LoadDiscordClients};
 use twilight_gateway::Shard;
 use twilight_model::gateway::Intents;
 use crate::bucket::Bucket;
 use crate::links::ScamLinks;
+
+async fn create_shard(
+    (id, value): (String, Client),
+    mongodb: MongoDBConnection,
+    redis: RedisConnection,
+    scam_domains: ScamLinks,
+    bucket: Bucket
+) {
+
+    let intents = Intents::MESSAGE_CONTENT | Intents::GUILD_MESSAGES | Intents::GUILDS | Intents::GUILD_BANS | Intents::GUILD_MEMBERS;
+
+    let (shard, mut events) = Shard::new(value.token.to_owned(), intents)
+        .await.unwrap();
+    shard.start().await.expect("Failed to start shard");
+    println!("Created shard for {}", id);
+
+    while let Some(event) = events.next().await {
+        tokio::spawn(on_event(
+            event,
+            mongodb.to_owned(),
+            redis.to_owned(),
+            value.http.to_owned(),
+            scam_domains.to_owned(),
+            bucket.to_owned()
+        ));
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -32,22 +60,30 @@ async fn main() {
 
     let discord_token =
         std::env::var("DISCORD_TOKEN").expect("Cannot load DISCORD_TOKEN from .env");
-    let discord_http = Arc::new(twilight_http::Client::new(discord_token.clone()));
 
-    let intents = Intents::MESSAGE_CONTENT | Intents::GUILD_MESSAGES | Intents::GUILDS | Intents::GUILD_BANS | Intents::GUILD_MEMBERS;
-    let (shard, mut events) = Shard::new(discord_token, intents).await.unwrap();
+    let discord_clients = DiscordClients::load(
+        &mongodb, None, None
+    ).await.unwrap();
 
-    shard.start().await.expect("Failed to start shard");
-    println!("Created shard");
-
-    while let Some(event) = events.next().await {
-        tokio::spawn(on_event(
-            event,
+    for value in discord_clients.iter() {
+        tokio::spawn(create_shard(
+            (value.key().to_string(), value.to_owned()),
             mongodb.to_owned(),
             redis.to_owned(),
-            discord_http.to_owned(),
             scam_domains.to_owned(),
             bucket.to_owned()
         ));
     }
+
+    create_shard(
+        ("main".to_string(), Client {
+            public_key: "".to_string(),
+            http: Arc::new(twilight_http::Client::new(discord_token.to_owned())),
+            token: discord_token
+        }),
+        mongodb.to_owned(),
+        redis.to_owned(),
+        scam_domains.to_owned(),
+        bucket.to_owned()
+    ).await;
 }
