cb63f4621e5bbd09c9d2869640c2b3fb589417ff
ocean <47253870+oceaann@users.noreply.github.com>
2022-06-27 20:11:48 +0200

    get_increase_bucket_amount method

--- database/src/models/config/automod/bucket.rs
+++ database/src/models/config/automod/bucket.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use twilight_model::channel::Message;
 use crate::models::config::automod::actions::Actions;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -6,15 +7,39 @@ use crate::models::config::automod::actions::Actions;
 pub enum IncreaseBucketAmount {
     Stickers,
     Attachments,
-    Mentions,
+    Mentions(MentionsCount),
     Static(u8)
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct BucketActions {
+pub struct MentionsCount {
+    count_replay: bool
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct BucketAction {
+    pub attached_rule_name: String,
     pub amount: IncreaseBucketAmount,
     pub actions: Vec<Actions>,
     pub reason: String,
-    // minimal value required to run action
+    /// minimal value required to run action
     pub min: u8
+}
+
+pub fn get_increase_bucket_amount(amount: IncreaseBucketAmount, message: &Message) -> usize {
+    match amount {
+        IncreaseBucketAmount::Stickers => message.sticker_items.len(),
+        IncreaseBucketAmount::Attachments => message.attachments.len(),
+        IncreaseBucketAmount::Mentions(count) => {
+            let mentions = message.mentions.len();
+            if !count.count_replay {
+                if let Some(reference) = &message.referenced_message {
+                    let include_mention = message.mentions.iter()
+                        .find(|m| m.id == reference.author.id).is_some();
+                    if include_mention { mentions - 1 } else { mentions }
+                } else { mentions }
+            } else { mentions }
+        },
+        IncreaseBucketAmount::Static(value) => value as usize
+    }
 }
