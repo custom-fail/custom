65ac93d5a77303837c6c06752d9da024b7628731
ocean <47253870+oceaann@users.noreply.github.com>
2022-06-27 12:14:31 +0200

    basic automod rules

--- database/src/models/config/automod/mod.rs
+++ database/src/models/config/automod/mod.rs
@@ -9,11 +9,20 @@ pub mod checks;
 pub mod filters;
 pub mod bucket;
 
+pub enum BasicAutoModerationRule {
+    ScamLinks = 1,
+    Mentions = 2,
+    CapsLock = 3,
+    Invites = 4
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct AutoModeratorV2 {
+pub struct AutoModeratorRule {
+    pub basic_type: Option<BasicAutoModerationRule>,
     pub filters: Vec<Filters>,
     pub checks: Vec<Checks>,
     pub actions: Vec<Actions>,
     pub ignore: Vec<Ignore>,
-    pub reason: String
+    pub reason: String,
+    pub name: String
 }
--- database/src/models/config/moderation.rs
+++ database/src/models/config/moderation.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 use twilight_model::id::Id;
 use twilight_model::id::marker::{ChannelMarker, RoleMarker, UserMarker};
-use crate::models::config::automod::AutoModeratorV2;
+use crate::models::config::automod::AutoModeratorRule;
 use crate::models::config::automod::bucket::BucketActions;
 use serde_repr::{Deserialize_repr, Serialize_repr};
 
@@ -30,6 +30,6 @@ pub struct Moderation {
     pub dm_case: bool,
     pub automod_logs: Option<Id<ChannelMarker>>,
     pub bucket_actions: HashMap<String, BucketActions>,
-    pub automod: Vec<AutoModeratorV2>,
+    pub automod: Vec<AutoModeratorRule>,
     pub automod_ignore: Vec<Ignore>
 }
