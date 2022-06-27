02a557fa69f7e2a35626450edf9e713f95dabef7
ocean <47253870+oceaann@users.noreply.github.com>
2022-06-27 12:36:28 +0200

    (fix): add missing macros

--- database/src/models/config/automod/mod.rs
+++ database/src/models/config/automod/mod.rs
@@ -3,12 +3,15 @@ use crate::models::config::automod::filters::Filters;
 use serde::{Serialize, Deserialize};
 use crate::models::config::automod::checks::Checks;
 use crate::models::config::moderation::Ignore;
+use serde_repr::{Serialize_repr, Deserialize_repr};
 
 pub mod actions;
 pub mod checks;
 pub mod filters;
 pub mod bucket;
 
+#[derive(Serialize_repr, Deserialize_repr, Debug, Clone, PartialEq)]
+#[repr(u8)]
 pub enum BasicAutoModerationRule {
     ScamLinks = 1,
     Mentions = 2,
