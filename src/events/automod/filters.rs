8e4e7c7f2794f3e95aeec2b15ec09ec1f6538e90
oceaann <47253870+oceaann@users.noreply.github.com>
2023-03-16 15:45:40 +0100

    Correct naming, and impl usage for filters and checks

--- src/events/automod/filters.rs
+++ src/events/automod/filters.rs
@@ -1,30 +1,33 @@
 use twilight_model::channel::Message;
-use crate::models::config::automod::filters::{Attachments, Filters, MessageLength};
+use crate::models::config::automod::filters::{Attachments, Filter, MessageLength};
 
-pub fn filters_match(filter: Filters, message: Message) -> bool {
-    match filter {
-        Filters::Attachments(config) => attachments(config, message),
-        Filters::MessageLength(config) => message_length(config, message),
-        Filters::Stickers => stickers(message)
+impl Filter {
+    pub fn is_matching(&self, message: &Message) -> bool {
+        match &self {
+            Filter::Attachments(config) => Self::attachments(config, message),
+            Filter::MessageLength(config) => Self::message_length(config, message),
+            Filter::Stickers => Self::stickers(message)
+        }
     }
+
+    fn attachments(config: &Attachments, message: &Message) -> bool {
+        let message_attachments_count = message.attachments.len();
+        (if let Some(min) = config.min {
+            (min as usize) > message_attachments_count
+        } else { false }) || (if let Some(max) = config.max {
+            (max as usize) < message_attachments_count
+        } else { false })
+    }
+
+    fn message_length(config: &MessageLength, message: &Message) -> bool {
+        let message_content_len = message.content.len();
+        (if let Some(min) = config.min {
+            (min as usize) > message_content_len
+        } else { false }) || (if let Some(max) = config.max {
+            (max as usize) < message_content_len
+        } else { false })
+    }
+
+    fn stickers(message: &Message) -> bool { !message.sticker_items.is_empty() }
 }
 
-fn attachments(config: Attachments, message: Message) -> bool {
-    let message_attachments_count = message.attachments.len();
-    (if let Some(min) = config.min {
-        (min as usize) > message_attachments_count
-    } else { false }) || (if let Some(max) = config.max {
-        (max as usize) < message_attachments_count
-    } else { false })
-}
-
-fn message_length(config: MessageLength, message: Message) -> bool {
-    let message_content_len = message.content.len();
-    (if let Some(min) = config.min {
-        (min as usize) > message_content_len
-    } else { false }) || (if let Some(max) = config.max {
-        (max as usize) < message_content_len
-    } else { false })
-}
-
-fn stickers(message: Message) -> bool { !message.sticker_items.is_empty() }
--- src/models/config/automod/filters.rs
+++ src/models/config/automod/filters.rs
@@ -1,20 +1,20 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 #[serde(tag = "type")]
-pub enum Filters {
+pub enum Filter {
     MessageLength(MessageLength),
     Attachments(Attachments),
     Stickers
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 pub struct MessageLength {
     pub min: Option<u16>,
     pub max: Option<u16>
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 pub struct Attachments {
     pub min: Option<u8>,
     pub max: Option<u8>
