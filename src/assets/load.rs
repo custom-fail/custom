db842f57364a29fa84e2dab9e55722d2b91bf011
oceaann <47253870+oceaann@users.noreply.github.com>
2023-05-04 11:59:57 +0100

    change yaml files to toml

--- src/assets/load.rs
+++ src/assets/load.rs
@@ -3,12 +3,12 @@ use tokio::fs;
 use super::Asset;
 
 pub async fn load(path: Option<String>) -> Asset {
-    let path = path.unwrap_or("messages.yaml".to_string());
-    let default_asset = fs::read(path.to_owned())
+    let path = path.unwrap_or("messages.toml".to_string());
+    let default_asset = fs::read_to_string(path.to_owned())
         .await.expect(format!("Cannot load {path}").as_str());
 
-    let default_asset: Asset = serde_yaml::from_slice(&default_asset)
+    let default_asset: Asset = toml::from_str(&default_asset)
         .expect(format!("Cannot parse {path}").as_str());
-
+    
     default_asset
 }
